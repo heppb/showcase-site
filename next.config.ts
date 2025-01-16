9bcc5af33560445c7ed863762e281e7cdcfdaa0f
heppb <22299591+heppb@users.noreply.github.com>
2025-01-16 15:22:42 -0500

    Cleaned up config file

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,3 @@
-import type { NextConfig } from "next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -13,26 +11,11 @@ const nextConfig = {
       },
     ],
   },
-};
-
-module.exports = {
-  reactStrictMode: true,
   env: {
     BASE_URL: process.env.BASE_URL,
     DISCOG_TOKEN: process.env.DISCOG_TOKEN,
     USER: process.env.USER,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.discogs.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
-}
+};
 
-
-export default nextConfig;
+module.exports = nextConfig;
