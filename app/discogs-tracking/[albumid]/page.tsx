b59b6afec6cab535c662269ae670e235163b372e
heppb <22299591+heppb@users.noreply.github.com>
2025-01-16 15:11:49 -0500

    Final change before changing to a squash commit branch

--- app/discogs-tracking/[albumid]/page.tsx
+++ app/discogs-tracking/[albumid]/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import RetrieveAlbumsClient from "./components/RetrieveAlbumsClient";
 import styles from "@/app/styles/Home.module.css";
 
+export const dynamic = "force-dynamic"; // Ensure dynamic rendering
+
 export default async function Page({ params }: { params: any }) {
   const { albumid } = params;
 
