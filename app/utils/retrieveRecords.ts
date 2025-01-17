273a468a175f4394895342091b9da3b720ba3eb5
heppb <22299591+heppb@users.noreply.github.com>
2025-01-17 13:42:52 -0500

    Echo debug yml

--- app/utils/retrieveRecords.ts
+++ app/utils/retrieveRecords.ts
@@ -3,8 +3,9 @@ import DiscogResponse from "../models/DiscogResponse";
 const retrieveRecords = async (): Promise<DiscogResponse | null> => {
   try {
     const user: string = localStorage.getItem('user') || (process.env.USER || "");
+    const token: string = process.env.DISCOG_TOKEN || "";
     const response = await fetch(
-      `https://api.discogs.com/users/${user}/collection/folders/0/releases?token=${process.env.DISCOG_TOKEN}&per_page=100&sort=artist`
+      `https://api.discogs.com/users/${user}/collection/folders/0/releases?token=${token}&per_page=100&sort=artist`
     );
 
     const data = (await response.json()) as DiscogResponse;
