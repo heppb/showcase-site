a71a94d66d7adf349b58ff619bd26353bc467ed6
heppb <22299591+heppb@users.noreply.github.com>
2025-01-21 11:48:21 -0500

    Next.config updates

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "i.discogs.com",
+        hostname: "**",
         port: "",
         pathname: "/**",
       },
